64d6a71be928498dfb2e47090e8a909f451825ea
berserker163 <berserker163@163.com>
2020-05-10 23:18:45 +0800

    restart C11

---
+++ cmake-build-debug/CMakeFiles/protype.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wangd/CLionProjects/protype/LBoost1/main.cpp" "/home/wangd/CLionProjects/protype/cmake-build-debug/CMakeFiles/protype.dir/LBoost1/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../LBoost1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/protype.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/protype.dir/LBoost1/main.cpp.o"
+  "protype"
+  "protype.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/protype.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
